68074fb82d3ee977d01e336a9547dae7e9d3d610
Nilisim <nil.bosch@enti.cat>
2025-05-09 23:13:11 +0200

    mapa_acabat_player_no_config_acabat

--- Projecte-GTA_SAN_ANDREAS.cpp
+++ Projecte-GTA_SAN_ANDREAS.cpp
@@ -6,143 +6,216 @@
 
 struct Config
 {
-    int ancho = 50;
-    int alto = 25;
-    int peatonesLosSantos;
-    int peajeLosSantos;
-    int dineroMaxLosSantos;
-    int peatonesSanFierro;
-    int peajeSanFierro;
-    int dineroMaxSanFierro;
+    int amplada;
+    int altura;
+    int peatonsLosSantos;
+    int peatgeLosSantos;
+    int dinersMaxLosSantos;
+    int peatonsSanFierro;
+    int peatgeSanFierro;
+    int dinersMaxSanFierro;
 };
 
-struct Posicion
+struct Posicio
 {
     int x;
     int y;
 };
 
-struct Peaton
+struct Peatons
 {
-    Posicion pos;
-    char direccion;
-    bool vivo;
+    Posicio posicio;
+    char direccio;
+    bool viu;
 };
 
 struct Jugador
 {
-    Posicion pos;
-    char mirandoHacia;
-    int dinero;
+    Posicio posicio;
+    Posicio posAnterior;
+    char Direccio;
+    int diners;
 };
 
 struct Dinero
 {
-    Posicion pos;
+    Posicio posicio;
     int cantidad;
 };
 
-class GTASANANDREAS
+struct GTASANANDREAS
 {
-private:
+
     Config config;
     char** mapa;
     Jugador jugador;
-    Peaton* peatones;
-    int cantidadPeatones;
-    Dinero* dineroEnSuelo;
-    int cantidadDinero;
-    int islaActual;
-    int anchoVista;
-    int altoVista;
+    int ampladaIsla;
+    Peatons* peatons;
+    int cantitatPeatons;
+    int ampladaVista;
+    int anchuraVista;
+    int FPS = 60;
 
-    void cargarConfiguracion()
+
+
+
+    GTASANANDREAS()
     {
+        cargarConfiguracio();
+        inicialitzarMapa();
+        inicialitzarJugador();
 
+        mostrarMapaInicial();
+        mapa[jugador.posicio.y][jugador.posicio.x] = jugador.Direccio;
+
+        while (true)
+        {
+            procesarInput();
+            actualizar();
+            Sleep(1);
+        }
+    }
+
+    ~GTASANANDREAS()
+    {
+        for (int i = 0; i < config.altura; i++)
+        {
+            delete[] mapa[i];
+        }
+        delete[] mapa;
+        delete[] peatons;
+
+    }
+
+    void cargarConfiguracio()
+    {
         std::ifstream archivo("config.txt");
         if (archivo.is_open())
         {
             char separador;
-            archivo >> config.ancho >> separador >> config.alto;
-            archivo >> config.peatonesLosSantos >> separador >> config.peajeLosSantos >> separador >> config.dineroMaxLosSantos;
-            archivo >> config.peatonesSanFierro >> separador >> config.peajeSanFierro >> separador >> config.dineroMaxSanFierro;
+            archivo >> config.amplada >> separador >> config.altura;
+            archivo >> config.peatonsLosSantos >> separador >> config.peatgeLosSantos >> separador >> config.dinersMaxLosSantos;
+            archivo >> config.peatonsSanFierro >> separador >> config.peatgeSanFierro >> separador >> config.dinersMaxSanFierro;
             archivo.close();
+        }
+        else
+        {
+            std::cout << "No es pot obrir el fitxer";
         }
     }
 
-    void inicializarMapa()
+    void inicialitzarMapa()
     {
-        mapa = new char* [config.alto];
-        for (int i = 0; i < config.alto; i++)
+        mapa = new char* [config.altura];
+
+        for (int i = 0; i < config.altura; i++)
         {
-            mapa[i] = new char[config.ancho];
-            for (int j = 0; j < config.ancho; j++)
+            mapa[i] = new char[config.amplada];
+
+
+            for (int j = 0; j < config.amplada; j++)
             {
                 mapa[i][j] = ' ';
             }
         }
 
 
-        int anchoIsla = config.ancho / 3;
-        for (int i = 0; i < config.alto; i++)
+
+        ampladaIsla = config.amplada / 3;
+        for (int i = 0; i < config.altura; i++)
         {
-            mapa[i][anchoIsla] = '|';
-            mapa[i][anchoIsla * 2] = '|';
+            mapa[i][ampladaIsla] = 'X';
+            mapa[i][ampladaIsla * 2] = 'X';
 
-
-            if (i >= config.alto / 2 - 1 && i <= config.alto / 2 + 1)
+            if (i >= config.altura / 2 - 1 && i <= config.altura / 2 + 1)
             {
-                mapa[i][anchoIsla] = '=';
-                mapa[i][anchoIsla * 2] = '=';
+                mapa[i][ampladaIsla] = ' ';
+                mapa[i][ampladaIsla * 2] = ' ';
             }
         }
 
-
-        for (int j = 0; j < config.ancho; j++)
+        for (int j = 0; j < config.amplada; j++)
         {
-            mapa[0][j] = '-';
-            mapa[config.alto - 1][j] = '-';
+            mapa[0][j] = 'X';
+            mapa[config.altura - 1][j] = 'X';
         }
     }
 
-    void mostrarMapa()
+    void inicialitzarJugador()
     {
+        jugador.posicio.x = config.amplada / 2;
+        jugador.posicio.y = config.altura / 2;
+        jugador.posAnterior = jugador.posicio;
+        jugador.Direccio = '^';
+        jugador.diners = 0;
+    }
 
-        std::cout << "                 GTA SAN ANDREAS" << std::endl;
-        for (int i = 0; i < config.alto; i++)
+    void procesarInput()
+    {
+        jugador.posAnterior = jugador.posicio;
+
+        if (GetAsyncKeyState('W') && jugador.posicio.y > 1)
         {
-            for (int j = 0; j < config.ancho; j++)
+            jugador.posicio.y--;
+            jugador.Direccio = '^';
+        }
+        else if (GetAsyncKeyState('S') && jugador.posicio.y < config.altura - 2)
+        {
+            jugador.posicio.y++;
+            jugador.Direccio = 'v';
+        }
+        else if (GetAsyncKeyState('A') && jugador.posicio.x > 1)
+        {
+            jugador.posicio.x--;
+            jugador.Direccio = '<';
+        }
+        else if (GetAsyncKeyState('D') && jugador.posicio.x < config.amplada - 2)
+        {
+            jugador.posicio.x++;
+            jugador.Direccio = '>';
+        }
+    }
+
+
+
+    void mostrarMapaInicial()
+    {
+        system("cls");
+        std::cout << "                 GTA SAN ANDREAS" << std::endl;
+
+        mapa[jugador.posAnterior.y][jugador.posAnterior.x] = ' '; //posicio anterior del jugador en blanc
+
+        char jugadorOriginal = mapa[jugador.posicio.y][jugador.posicio.x];
+        mapa[jugador.posicio.y][jugador.posicio.x] = jugador.Direccio;
+
+        for (int i = 0; i < config.altura; i++)
+        {
+            for (int j = 0; j < config.amplada; j++)
             {
                 std::cout << mapa[i][j];
             }
             std::cout << '\n';
         }
+
+
+        mapa[jugador.posicio.y][jugador.posicio.x] = jugadorOriginal;
+
+        std::cout << "Diners: " << jugador.diners << std::endl;
     }
 
-public:
-    GTASANANDREAS()
+    void actualizar()
     {
-        cargarConfiguracion();
-        inicializarMapa();
-        mostrarMapa();
+        procesarInput();
+        mostrarMapaInicial();
 
     }
 
-    ~GTASANANDREAS()
-    {
-        for (int i = 0; i < config.alto; i++)
-        {
-            delete[] mapa[i];
-        }
-        delete[] mapa;
-        delete[] peatones;
-        delete[] dineroEnSuelo;
-    }
+
+
 };
 
 int main()
 {
     GTASANANDREAS juego;
-
 
 }
